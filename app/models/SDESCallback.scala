f4a2d0b70e8e2f4cc9692451945a776fc5b6e9ea
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2022-02-02 12:49:06 +0000

    [PRM-1005] Added model and enum for SDES Callback (#11)
    * [PRM-1005] Added model and enum for SDES Callback
    
    * [PRM-1005] reviews implemented

---
+++ app/models/SDESCallback.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.libs.json.{Json, Reads}
+
+import java.time.LocalDateTime
+
+
+case class SDESCallback(
+                         notification: SDESFileNotificationEnum.Value,
+                         filename: String,
+                         correlationID: String,
+                         checksumAlgorithm: Option[String],
+                         checksum: Option[String],
+                         availableUntil: Option[LocalDateTime] = None,
+                         failureReason: Option[String],
+                         dateTime: Option[LocalDateTime] = None,
+                         properties: Option[Seq[Properties]]
+                       )
+case class Properties(name: String, value: String)
+
+object SDESCallback {
+  implicit val format: Reads[SDESCallback] = Json.reads[SDESCallback]
+}
+
+object Properties {
+  implicit val formatProperties: Reads[Properties] = Json.reads[Properties]
+}
---
+++ test/models/SDESFileNotificationEnumSpec.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.{JsString, Json}
+
+class SDESFileNotificationEnumSpec extends AnyWordSpec with Matchers {
+
+  "be readable from JSON for 'FileReady'" in {
+    val result = Json.fromJson(JsString("FileReady"))(SDESFileNotificationEnum.format)
+    result.isSuccess shouldBe true
+    result.get shouldBe SDESFileNotificationEnum.FileReady
+  }
+
+  "be readable from JSON for 'FileReceived'" in {
+    val result = Json.fromJson(JsString("FileReceived"))(SDESFileNotificationEnum.format)
+    result.isSuccess shouldBe true
+    result.get shouldBe SDESFileNotificationEnum.FileReceived
+  }
+
+  "be readable from JSON for 'FileProcessingFailure'" in {
+    val result = Json.fromJson(JsString("FileProcessingFailure"))(SDESFileNotificationEnum.format)
+    result.isSuccess shouldBe true
+    result.get shouldBe SDESFileNotificationEnum.FileProcessingFailure
+  }
+
+  "be readable from JSON for 'FileProcessed'" in {
+    val result = Json.fromJson(JsString("FileProcessed"))(SDESFileNotificationEnum.format)
+    result.isSuccess shouldBe true
+    result.get shouldBe SDESFileNotificationEnum.FileProcessed
+  }
+}
