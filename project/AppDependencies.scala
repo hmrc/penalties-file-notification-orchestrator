838dc7caf81ece32cf5f5fc3f61bb86078dda820
Jack Blacklaws <58850837+JBlacklaws@users.noreply.github.com>
2022-09-09 09:12:57 +0100

    [PRM-1751] Updated dependencies (#25)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.0.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.2.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.70.0",
     "com.enragedginger"       %% "akka-quartz-scheduler"      % "1.9.2-akka-2.6.x"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"   % "7.0.0"    % "test",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"     % "0.70.0"    % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"   % "7.2.0"    % "test",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"     % "0.71.0"    % "test, it",
     "org.pegdown"             %  "pegdown"                     % "1.6.0"     % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"          % "5.1.0"     % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"               % "2.26.3"    % "it",
