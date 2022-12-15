2585e8520e667a906bb44794b3e4a579a0f9c546
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-12-15 09:08:23 +0000

    [PRM-2117] - Added test-only controller to retrieve bearer tokens

---
+++ app/controllers/testOnly/BearerTokenController.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.testOnly
+
+import play.api.Configuration
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.Inject
+
+class BearerTokenController @Inject()(cc: ControllerComponents, config: Configuration) extends BackendController(cc) {
+
+  def getBearerToken(serviceName: String): Action[AnyContent] = Action {
+    val optBearerToken = config.getOptional[String](s"$serviceName.outboundBearerToken")
+    optBearerToken.fold(NotFound(s"Bearer token not found for service: $serviceName"))(Ok(_))
+  }
+}
