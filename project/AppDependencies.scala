90570ce153334da7e118309ccecdf6ecb2ed57bd
Roheel Arshad <48284720+RoheelArshad@users.noreply.github.com>
2023-07-13 09:40:06 +0100

    PRM-2733 uplifted dependencies (#55)
    * PRM-2733 uplifted dependencies
    
    * PRM-2733 removed unnecessary config values
    
    * PRM-2733 added config removal as tests fail
    
    * PRM-2733 removed unnecessary config values

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.13.0"
+  val bootstrapVersion = "7.19.0"
   val hmrcMongoVersion = "0.74.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"                % hmrcMongoVersion,
     "com.enragedginger"       %% "akka-quartz-scheduler"             % "1.9.3-akka-2.6.x",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-28"      % "1.4.0"
+    "uk.gov.hmrc"             %% "internal-auth-client-play-28"      % "1.6.0"
   )
 
   val test = Seq(
