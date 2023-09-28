2d04cc9c0017469168c47abf5917a892f65db504
JBlacklaws <58850837+JBlacklaws@users.noreply.github.com>
2023-09-28 14:50:44 +0100

    [PRM-2934] Added endpoint for checking number of files in repository

---
+++ test/controllers/testOnly/MongoQueryControllerISpec.scala
@@ -0,0 +1,30 @@
+
+package controllers.testOnly
+
+import base.SpecBase
+import play.api.http.Status
+import play.api.http.Status.OK
+import play.api.mvc.Result
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, status, stubControllerComponents}
+import repositories.FileNotificationRepository
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class MongoQueryControllerISpec extends SpecBase {
+  val mockRepo: FileNotificationRepository = mock[FileNotificationRepository]
+  implicit val ec: ExecutionContext = injector.instanceOf[ExecutionContext]
+
+  class Setup {
+    reset(mockRepo)
+    val controller = new MongoQueryController(stubControllerComponents(), mockRepo)
+  }
+
+  "getNumberOfRecords" should {
+    s"return OK (${Status.OK}) with correct number of records" in new Setup {
+      when(mockRepo.counterAllRecords()).thenReturn(Future.successful(5))
+      val result: Future[Result] = controller.getNumberOfRecords()(fakeRequest)
+      status(result) shouldBe OK
+      contentAsString(result) shouldBe "5"
+    }
+  }
+}
