a355151636bac9d0ce62b44e187ff65d81ab3d61
JBlacklaws <58850837+JBlacklaws@users.noreply.github.com>
2024-01-02 16:04:47 +0000

    [PRM-3199] [WIP] Play 30 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"      % bootstrapVersion    % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"      % bootstrapVersion    % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"     % hmrcMongoVersion    % "test, it",
     "org.mockito"             %% "mockito-scala-scalatest"     % "1.17.29"           % "it, test"
   )
