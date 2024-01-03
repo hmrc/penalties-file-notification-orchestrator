b3d77fef13994e76f4b9fc5f8cbd761f303cf724
JBlacklaws <58850837+JBlacklaws@users.noreply.github.com>
2024-01-03 12:11:50 +0000

    [PRM-3199] Moved back to 1.4.0 for hmrc mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object AppDependencies {
 
   val bootstrapVersion = "8.2.0"
-  val hmrcMongoVersion = "1.6.0"
+  val hmrcMongoVersion = "1.4.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVersion,
